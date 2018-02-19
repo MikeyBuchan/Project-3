a821a207b6435cf1876c8049eeed9a6abdb2cc2f
Mas Tijben <Whosobvious@gmail.com>
2018-02-19 12:53:18 +0100

    oioi cunts
    i did some movement and camera shizzle wizzle

---
+++ Unity/Template/Assets/Scripts/T_CameraZoom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_CameraZoom : MonoBehaviour
+{
+    public float cameraZoomSpeed;
+    public Vector3 clamped;
+
+    public Transform player;
+
+    public int minZoomDistance;
+    public int maxZoomDistance;
+
+    private void Start()
+    {
+        clamped.z *= cameraZoomSpeed;
+    }
+
+    void Update()
+    {
+        //makes you zoom in when you scroll forwards.
+        if (Vector3.Distance(transform.position, player.position) > minZoomDistance && Input.GetAxis("Mouse ScrollWheel") >= 0)
+        {
+            transform.Translate(Input.GetAxis("Mouse ScrollWheel") * clamped * Time.deltaTime, Space.Self);
+        }
+
+        //makes you zoom out when you scroll backwards.
+        if (Vector3.Distance(transform.position, player.position) < maxZoomDistance && Input.GetAxis("Mouse ScrollWheel") <= 0)
+        {
+            transform.Translate(Input.GetAxis("Mouse ScrollWheel") * clamped * Time.deltaTime, Space.Self);
+        }
+    }
+}
---
+++ Unity/Template/Assets/Scripts/T_MovementScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_MovementScript : MonoBehaviour
+{
+    public GameObject characterCam;
+    private Vector3 cameraRotate;
+    public float cameraRotateSpeed;
+
+    private Vector3 movement;
+    public float movementSpeed;
+
+    void Start()
+    {
+        cameraRotate.y = cameraRotateSpeed;
+    }
+
+    void Update()
+    {
+        //rotates the camera when pressing Q or E.
+        if (Input.GetButton("E") == true && Input.GetButton("Q") == false)
+        {
+            transform.Rotate(-cameraRotate * Time.deltaTime);
+        }
+        if (Input.GetButton("Q") == true && Input.GetButton("E") == false)
+        {
+            transform.Rotate(cameraRotate * Time.deltaTime);
+        }
+
+        //moves the character using wasd.
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.Self);
+    }
+}
