b95d13acd7d0a01fd15c75869d61dcc489e0a284
Mas Tijben <Whosobvious@gmail.com>
2018-02-24 16:39:31 +0100

    did part of the store

---
+++ Unity/Template/Assets/Scripts/T_ShopScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class T_ShopScript : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject shopCamera;
+    public float shopRange;
+
+    private bool inShop = false;
+    public GameObject nearShopText;
+
+	void Start () 
+	{
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update () 
+	{
+		if(Vector3.Distance(transform.position,player.transform.position) <= shopRange)
+        {
+            nearShopText.SetActive(true);
+
+            if(Input.GetButtonDown("Jump") == true && inShop == false)
+            {
+                shopCamera.SetActive(true);
+                player.SetActive(false);
+                inShop = true;
+
+                nearShopText.GetComponent<Text>().text = "Press space to exit shop";
+            }         
+            else if(Input.GetButtonDown("Jump") == true && inShop == true)
+            {
+                player.SetActive(true);
+                shopCamera.SetActive(false);
+                inShop = false;
+
+                nearShopText.GetComponent<Text>().text = "Press space to enter shop";
+            }
+        }
+        else if(Vector3.Distance(transform.position, player.transform.position) > shopRange)
+        {
+            nearShopText.SetActive(false);
+        }
+	}
+}
