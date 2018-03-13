b640ee95258a97d96eb801cacc87d469b8275384
MikeyBuchan <psmbgames@hotmail.com>
2018-03-13 13:42:14 +0100

    test push
    nog is een test push

---
+++ Unity/Template/Assets/Scripts/Enemy/B_EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class B_EnemyMovement : MonoBehaviour 
+{
+    public NavMeshAgent agent;
+    public Transform navTarget;
+    private Vector3 v = new Vector3(0, 0, 1);
+    public float movSpeed;
+
+	void Start () 
+	{
+        agent = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+
+	void Update () 
+	{
+        agent.SetDestination(navTarget.position);
+        transform.Translate(v * movSpeed * Time.deltaTime);
+	}
+}
