96fb7295778bd1889eb1cc9223e3464e9c6cb1f7
JermovGroos <jvgroos@hotmail.nl>
2018-03-13 15:43:13 +0100

    Water script
    bezig met waterscript alleen textures van ability werken nog niet optimaal

---
+++ Unity/Template/Assets/Scripts/G_AgentScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G_AgentScript : MonoBehaviour {
+	public Transform target;
+	UnityEngine.AI.NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	agent.SetDestination(target.position);	
+	}
+}
---
+++ Unity/Template/Assets/Scripts/G_WaterScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class G_WaterScript : MonoBehaviour {
+	public float slowedSpeed;
+	private NavMeshAgent agent;
+	public float cooldownTimer;
+	public float nextUseTimer;
+	public Texture2D ability1;
+	public Texture2D ability1CD;
+	float ability1Timer = 0;
+	public float ability1CDtimer;
+
+	// Use this for initialization
+	void Start () {
+		agent = gameObject.GetComponent<NavMeshAgent>();
+		ability1Timer -= Time.deltaTime;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextUseTimer) {
+		if (Input.GetButtonDown ("Ability 1")){
+		agent.speed = slowedSpeed;
+		nextUseTimer = Time.time + cooldownTimer;
+		}
+		}
+	}
+	void OnGUI () {
+		bool ability1Button = Input.GetButtonDown ("Ability 1");
+		if (ability1Timer <= 0 )
+		{
+			GUI.Label(new Rect( 10, 10, 50, 50, ability1));
+					if (ability1Button) {
+				AbilityOne () ;
+				}
+		} else {
+			GUI.Label(new Rect( 10, 10, 50, 50, ability1));
+			}
+		}
+	void AbilityOne () {
+	}
+}
