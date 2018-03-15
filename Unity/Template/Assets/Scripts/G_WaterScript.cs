d58b9cec633bb53c5cf3c4c8079a76b813e77f49
JermovGroos <jvgroos@hotmail.nl>
2018-03-15 13:10:52 +0100

    Fixed water script
    fixed water script with images in right folders etc

--- Unity/Template/Assets/Scripts/G_WaterScript.cs
+++ Unity/Template/Assets/Scripts/G_WaterScript.cs
@@ -15,32 +15,32 @@ public class G_WaterScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		agent = gameObject.GetComponent<NavMeshAgent>();
-		ability1Timer -= Time.deltaTime;
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > nextUseTimer) {
 		if (Input.GetButtonDown ("Ability 1")){
+		agent = gameObject.GetComponent<NavMeshAgent>();
 		agent.speed = slowedSpeed;
 		nextUseTimer = Time.time + cooldownTimer;
-		}
+		ability1Timer = ability1CDtimer;
+		ability1Timer -= Time.deltaTime;
+			}
 		}
 	}
 	void OnGUI () {
 		bool ability1Button = Input.GetButtonDown ("Ability 1");
 		if (ability1Timer <= 0 )
 		{
-			GUI.Label(new Rect( 10, 10, 50, 50, ability1));
+			GUI.Label(new Rect( 10, 10, 50, 50), ability1);
 					if (ability1Button) {
 				AbilityOne () ;
 				}
 		} else {
-			GUI.Label(new Rect( 10, 10, 50, 50, ability1));
+			GUI.Label(new Rect( 10, 10, 50, 50), ability1CD);
 			}
 		}
 	void AbilityOne () {
+			}
 	}
-}
