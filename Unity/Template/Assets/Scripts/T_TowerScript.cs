aacad8d8650b69763b6cfe8d77bdc0149a18c44f
Mas Tijben <Whosobvious@gmail.com>
2018-03-20 13:33:49 +0100

    added basic tower script
    it needs a bug fix

---
+++ Unity/Template/Assets/Scripts/T_TowerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_TowerScript : MonoBehaviour
+{
+    public GameObject closestEnemy;
+    public GameObject secondEnemy;
+
+    private int enemiesInRange;
+
+    private void OnTriggerEnter(Collider c)
+    {      
+        if(c.gameObject.tag == "Enemy" && closestEnemy == null)
+        {
+            closestEnemy = c.gameObject;
+            enemiesInRange++;
+        }
+        else if(c.gameObject.tag == "Enemy" && closestEnemy != null)
+        {
+            secondEnemy = c.gameObject;
+            enemiesInRange++;
+        }
+
+        if (secondEnemy != null && closestEnemy != null && Vector3.Distance(transform.position, secondEnemy.transform.position) > Vector3.Distance(transform.position, closestEnemy.transform.position))
+        {
+            closestEnemy = secondEnemy;
+            secondEnemy = null;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            enemiesInRange--;
+        }
+    }
+
+    void Update () 
+	{
+        if(enemiesInRange >= 1)
+        {
+            transform.LookAt(closestEnemy.transform);
+        }
+	} 
+}
