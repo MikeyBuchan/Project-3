2aa8e1bf3f005735716c1251f237be10b1b2d204
MikeyBuchan <psmbgames@hotmail.com>
2018-04-22 20:47:10 +0200

    Fixed alot of shit
    ^^^^^^^^^^^^^^^

---
+++ Unity/Template/Assets/Scripts/B_DownBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_DownBox : MonoBehaviour 
+{
+    public GameObject player;
+    public Vector3 respawnLoc;
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            player.transform.position = respawnLoc;
+        }
+        else if (c.gameObject.tag == "Enemy")
+        {
+            //GameObject.FindGameObjectWithTag
+        }
+    }
+}
---
+++ Unity/Template/Assets/Scripts/B_WindPush.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_WindPush : MonoBehaviour 
+{
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+}
